d1a3cf18e511e4387d1a4e21654b32e2b38c7789
Slimboyfat-1979 <slimboyfat1979@gmail.com>
2025-02-11 20:54:01 +1300

    More shitttt

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express, { response } from 'express';
 import path from 'path';
 import { fileURLToPath} from 'url';
 import { dirname } from 'path';
+import dotenv from 'dotenv';
+import { createClient } from '@supabase/supabase-js';
+
+// dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,16 +18,16 @@ app.listen(3000, () => {
     console.log('Listening on port 3000');
 })
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => {  
     res.sendFile(path.join(__dirname, 'index.html')); 
 })
 
 app.get("/view/:id", (req, res) => {
     const id = req.params.id;
     res.sendFile(path.join(__dirname, 'public', 'view.html'))
-    // fetch(`http://localhost:4000/${id}`).then(response => response.json()).then(data => {
-    //     console.log(data);
-    // });
+})
 
+app.get("/addpost", (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'addpost.html'))
 })
 
